03ecd225b666fe19e194f7034cee2100ef4ebd04
May Jimenez <may_coloresjp@hotmail.com>
2022-09-15 23:30:45 +0200

    templates con base y extends

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,6 +1,12 @@
 from registro_ig import app
+from flask import render_template
 
 
 @app.route("/")
 def index():
-    return "Servidor rulando"
+    return render_template("index.html", pageTitle="Lista")
+
+
+@app.route("/nuevo")
+def alta():
+    return render_template("new.html", pageTitle="Alta")   
